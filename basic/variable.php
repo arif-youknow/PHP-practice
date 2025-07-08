650b3d921891435f2ecc583cc71a121ca510f48a
Md. Arif Khan <101652388+arif-youknow@users.noreply.github.com>
2025-07-08 18:03:34 +0000

    variable

---
+++ basic/variable.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "I am learning php basic\n";
+
+$myName = "md. arif khan";
+$myAge = 24;
+$isMale = true;
+
+echo $myName . "\n"; //concatinate(.)
+
+echo $myAge . "\n";
+echo $isMale . "\n";
+
+var_dump($myName); //shows datatype and value
+
+echo " My name is $myName \n I'm $myAge years old \n";
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-echo "Hello\n";
-echo "Bangladesh"
-
-?>
